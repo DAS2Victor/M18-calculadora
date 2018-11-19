970473bda1c3f10e18339ef1543b3e1016858911
Victor <vboix2@insgabrielamistral.cat>
2018-11-19 19:34:28 +0100

    Afegir classe Calculadora

---
+++ src/funcions/Calculadora.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package funcions;
+
+/**
+ *
+ * @author victor
+ */
+public class Calculadora {
+    public static int calculOperacionsEnters(String s){
+        //Rep una cadena de text amb una operació
+        //Valida l'operació i retorna el resultat
+        
+        return 0;
+    } 
+}
